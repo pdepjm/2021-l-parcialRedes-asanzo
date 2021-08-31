cdfba6cf8900de481e70ff59cfca2d8169e917e5
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2021-08-31 18:40:11 -0300

    Agrego resp teoricas Fixes #1

--- programa.pl
+++ programa.pl
@@ -1,3 +1,7 @@
+% Enunciado:
+% https://docs.google.com/document/u/4/d/e/2PACX-1vQU1UfSb5E1UGRtuaTmdksu8my4TlvfHOwET2cNKlwgp_5knH85H-lcsCtlurpKXn5vKF_RNSQTgxKw/pub
+
+%%%% PUNTO 1 %%%%
 
 canal(ana, youtube, 3000000).
 canal(ana, instagram, 2700000).
@@ -9,6 +13,8 @@ canal(beto, instagram, 1100000).
 canal(cami, tiktok, 2000).
 canal(dani, youtube, 1000000).
 canal(evelyn, instagram, 1).
+
+%%%% PUNTO 2A %%%%
 
 % influencer/1 se cumple para un usuario que tiene más de 10.000 seguidores en total entre todas sus redes.
 
@@ -28,6 +34,8 @@ usuario(Quien) :- distinct(Quien,canal(Quien, _, _)).
 :- end_tests(influencer).
 
 
+%%%% PUNTO 2B %%%%
+
 /*
 omnipresente/1 se cumple para un influencer si está en cada red que existe (se consideran como existentes aquellas redes en las que haya al menos un usuario).
 Por ejemplo, ana es omnipresente.
@@ -45,6 +53,8 @@ red(Red):- distinct(Red, canal(_, Red, _)).
    omnipresente(Omni).
  
 :- end_tests(omnipresente).
+
+%%%% PUNTO 2C %%%%
 
 /*exclusivo/1 se cumple cuando un influencer sólo está en una red. 
 Por ejemplo, dani
@@ -66,6 +76,7 @@ estaEnDosRedes(Quien):-
  
 :- end_tests(exclusivo).
 
+%%%% PUNTO 3A %%%%
 
 publicacion(ana, tiktok, video([beto, evelyn], 1)).
 publicacion(ana, tiktok, video([ana], 1)).
@@ -75,9 +86,12 @@ publicacion(cami, youtube, video([cami], 5)).
 publicacion(cami, twitch, stream(leagueOfLegends)).
 publicacion(evelyn, instagram, foto([cami, evelyn])).
 
+%%%% PUNTO 3B %%%%
 esJuego(leagueOfLegends).
 esJuego(minecraft).
 esJuego(aoe).
+
+%%%% PUNTO 4 %%%%
 
 /*
 adictiva/1 se cumple para una red cuando sólo tiene contenidos adictivos (Un contenido adictivo es un video de menos de 3 minutos, un stream sobre una temática relacionada con juegos, o una foto con menos de 4 participantes).
@@ -102,6 +116,8 @@ esAdictivo(foto(Quienes)):-
  
 :- end_tests(adictiva).
 
+%%%% PUNTO 5 %%%%
+
 /*
 colaboran/2 se cumple cuando un usuario aparece en las redes de otro (en alguno de sus contenidos). En un stream aparece sólo quien creó el contenido.
  Esta relación debe ser simétrica. (O sea, si a colaboró con b, entonces también debe ser cierto que b colaboró con a)*/
@@ -121,6 +137,7 @@ estaEn(_, video(Quienes,_), Quien):-
     member(Quien, Quienes).
 estaEn(Autor, stream(_), Autor).
 
+%%%% PUNTO 6 %%%%
 
 /*
 caminoALaFama/1 se cumple para un usuario no influencer cuando un influencer publicó contenido en el que aparece el usuario, o bien el influencer publicó contenido donde aparece otro usuario que a su vez publicó contenido donde aparece el usuario. Debe valer para cualquier nivel de indirección.
@@ -176,3 +193,18 @@ caminoALaFamaV3(Usuario):-
    caminoALaFama(Caminante).
  
 :- end_tests(caminoALaFama).
+
+%%%% PUNTO 7 %%%%
+
+/*
+    Hacer al menos un test que pruebe que una consulta existencial sobre alguno de los puntos funcione correctamente.
+
+    (HECHOS)
+*/
+
+/*
+    ¿Qué hubo que hacer para modelar que beto no tiene tiktok? Justificar conceptualmente.
+
+    Respuesta:
+    No hubo que agregar ninguna línea de código. Quien consulte ?- canal(beto, tiktok, _). obtendrá falso por el concepto de universo cerrado: todo lo que no esté en la base de conocimientos es falso. Entonces, al no agregarlo ya estoy diciendo "beto no tiene tiktok" implícitamente.
+*/
